385ad257b0185f12f0f75558db41ab17f928455b
OhFig <60785894+OhFig@users.noreply.github.com>
2020-02-07 11:22:33 -0600

    Cards.java

---
+++ Cards.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class Cards {
+	public static ArrayList<Integer> deck = new ArrayList<Integer>(14);
+
+public void Cards() {
+
+		deck.add(0, 4);
+		deck.add(1, 4);
+		deck.add(2, 4);
+		deck.add(3, 4);
+		deck.add(4, 4);
+		deck.add(5, 4);
+		deck.add(6, 4);
+		deck.add(7, 4);
+		deck.add(8, 4);
+		deck.add(9, 4);
+		deck.add(10, 4);
+		deck.add(11, 4);
+		deck.add(12, 4);
+
+}
+}
